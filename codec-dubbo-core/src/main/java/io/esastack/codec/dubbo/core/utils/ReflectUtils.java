428586cd730049c1963a24cfd37743c223729718
guconglin <guconglin@oppo.com>
2021-07-13 17:32:07 +0800

    Remove useless code

--- codec-dubbo-core/src/main/java/io/esastack/codec/dubbo/core/utils/ReflectUtils.java
+++ codec-dubbo-core/src/main/java/io/esastack/codec/dubbo/core/utils/ReflectUtils.java
@@ -114,7 +114,6 @@ public final class ReflectUtils {
         StringBuilder ret = new StringBuilder();
 
         while (c.isArray()) {
-            isArray = true;
             ret.append('[');
             c = c.getComponentType();
         }
